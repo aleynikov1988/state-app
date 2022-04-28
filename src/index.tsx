46514ddb97132c1b87ef94650d4041ee34619aae
Maxim Aleynikov <max.aleyinikov@gmail.com>
2022-04-28 12:00:18 +0300

    some refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import App from "./components/App";
+import App from "./App";
 import store from './store';
 
 const root = ReactDOM.createRoot(
